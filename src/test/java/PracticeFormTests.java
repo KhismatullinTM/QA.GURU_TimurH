a56248d5a54450bed69fd8479e091e2d0fe963f1
timur_khismatullin <timuuur01@yandex.ru>
2026-07-11 21:49:22 +0500

    Доделал автотест validTest на проверку формы PracticeForm. Добавил проверки таблицы результата заполнения формы.

--- src/test/java/PracticeFormTests.java
+++ src/test/java/PracticeFormTests.java
@@ -2,8 +2,7 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import java.io.File;
-
+import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
@@ -17,6 +16,9 @@ public class PracticeFormTests {
     String invalidPhoneNumber = "965789543";
     String subjects = "Computer Science";
     String hobby = "Sports";
+    String currentAddress = "1 Kremlevskaya Embankment, Moscow, Russian Federation";
+    String userState = "Rajasthan";
+    String userCity = "Jaipur";
 
     @BeforeEach
     void setUp (){
@@ -36,13 +38,33 @@ public class PracticeFormTests {
         $("[id=userEmail]").setValue(email);
         $("[id=gender-radio-1][value=Male]").click();
         $("[id=userNumber]").setValue(phoneNumber);
+        $("[id=dateOfBirthInput]").click();
+        $("[class=react-datepicker__year-select]").selectOption("2000");
+        $("[class=react-datepicker__month-select]").selectOption("January");
+        $(".react-datepicker__day.react-datepicker__day--001").click();
         $("[id=subjectsContainer]").click();
         $("[id=subjectsInput]").setValue(subjects);
         $(byText(subjects)).click();
         $(byText(hobby)).click();
-        $("[id=uploadPicture]").uploadFile(new File("src/test/resources/Dubai.jpg"));
-        $("[id=submit]").scrollTo().click(); // У меня небольшая диагональ экрана. Хотя Станислав говорил, что это стрем, но пока что я альтернатив не изучил)
+        $("[id=uploadPicture]").uploadFromClasspath("Dubai.jpg");
+        $("[id=currentAddress]").scrollTo().setValue(currentAddress);
+        $("#react-select-3-input").setValue(userState).pressEnter();
+        $("#react-select-4-input").setValue(userCity).pressEnter();
+        $("[id=submit]").click();
         $("[id=example-modal-sizes-title-lg]").shouldBe(visible);
+
+        // Проверки
+        $("[class=table-responsive]").$(byText("Student Name")).parent().shouldHave(text(userName), text(userLastName));
+        $("[class=table-responsive]").$(byText("Student Email")).parent().shouldHave(text(email));
+        $("[class=table-responsive]").$(byText("Gender")).parent().shouldHave(text("Male"));
+        $("[class=table-responsive]").$(byText("Mobile")).parent().shouldHave(text(phoneNumber));
+        $("[class=table-responsive]").$(byText("Date of Birth")).parent().shouldHave(text("1 January,2000"));
+        $("[class=table-responsive]").$(byText("Subjects")).parent().shouldHave(text(subjects));
+        $("[class=table-responsive]").$(byText("Hobbies")).parent().shouldHave(text(hobby));
+        $("[class=table-responsive]").$(byText("Picture")).parent().shouldHave(text("Dubai.jpg"));
+        $("[class=table-responsive]").$(byText("Address")).parent().shouldHave(text(currentAddress));
+        $("[class=table-responsive]").$(byText("State and City")).parent().shouldHave(text(userState), text(userCity));
+
     }
 
     @Test
