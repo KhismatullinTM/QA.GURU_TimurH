412f89ef68a0d4ba842f828479b9701937fa9784
timur_khismatullin <timuuur01@yandex.ru>
2026-07-12 15:52:23 +0500

    Изменил модификаторы доступа переменным в классе TestBase

--- src/test/java/TestBase.java
+++ src/test/java/TestBase.java
@@ -6,23 +6,23 @@ import static com.codeborne.selenide.Selenide.closeWebDriver;
 
 public class TestBase {
 
-    public static final String userName = "Timur";
-    public static final String userLastName = "T";
-    public static final String fullUserName = "Timur T";
-    public static final String email = "avito9196@yandex.ru";
-    public static final String phoneNumber = "9657895436";
-    public static final String gender = "Male";
-    public static final String invalidPhoneNumber = "965789543";
-    public static final String subjects = "Computer Science";
-    public static final String hobby = "Sports";
-    public static final String currentAddress = "1 Kremlevskaya Embankment, Moscow, Russian Federation";
-    public static final String userState = "Rajasthan";
-    public static final String userCity = "Jaipur";
+    protected static final String userName = "Timur";
+    protected static final String userLastName = "T";
+    protected static final String fullUserName = "Timur T";
+    protected static final String email = "avito9196@yandex.ru";
+    protected static final String phoneNumber = "9657895436";
+    protected static final String gender = "Male";
+    protected static final String invalidPhoneNumber = "965789543";
+    protected static final String subjects = "Computer Science";
+    protected static final String hobby = "Sports";
+    protected static final String currentAddress = "1 Kremlevskaya Embankment, Moscow, Russian Federation";
+    protected static final String userState = "Rajasthan";
+    protected static final String userCity = "Jaipur";
 
-    public static final String secondFullUserName = "Eva Nikonova";
-    public static final String secondEmail = "evanikonova@gmail.com";
-    public static final String invalidSecondEmail = "evanikonovagmail.com";
-    public static final String secondCurrentAddress = "2 Palace Square, Saint Petersburg, Russian Federation";
+    protected static final String secondFullUserName = "Eva Nikonova";
+    protected static final String secondEmail = "evanikonova@gmail.com";
+    protected static final String invalidSecondEmail = "evanikonovagmail.com";
+    protected static final String secondCurrentAddress = "2 Palace Square, Saint Petersburg, Russian Federation";
 
     @BeforeEach
     void setUp (){
